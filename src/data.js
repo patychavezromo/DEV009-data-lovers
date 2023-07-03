9ed7c22f80bb3dc772399be0aefb872e53342bde
Andrea Cabrera <andreacabrera@MacBook-Air-de-Andrea-2.local>
2023-07-03 17:37:10 -0600

    Se agregan funcionalidades para los filtros

--- src/data.js
+++ src/data.js
@@ -1,9 +1,82 @@
-// estas funciones son de ejemplo
+const dataFunctions= {
 
-export const example = () => {
-  return 'example';
+
+  femaleFilter: (list) =>{
+    const femaleAthletes = list.filter(element => element.gender == "F");
+    return femaleAthletes;
+  },
+
+  maleFilter: (list) =>{
+    const maleAthletes = list.filter(element => element.gender == "M");
+    return maleAthletes;
+  },
+
+  teamFilter: (list, selectedOption) =>{
+    const teams = list.filter(element => element.team == selectedOption);
+    return teams;
+  },
+  
+  sportFilter: (list, selectedOption) =>{
+    const sports = list.filter(element => element.sport == selectedOption);
+    return sports;
+  },
+
+  showAllCards: (athletesList)=>{
+    const root = document.getElementById('root');
+ 
+    for(let i=0; i<athletesList.length; i++){
+      let card= document.createElement('div');
+      card.className= 'card';
+      let content= document.createElement('div');
+      content.className= 'content';
+
+      let name= document.createElement('p');
+      name.innerText= 'Nombre: '+athletesList[i].name;
+      content.appendChild(name);
+
+      let gender= document.createElement('p');
+      gender.innerText= 'Género: '+athletesList[i].gender; 
+      content.appendChild(gender);
+
+      let height= document.createElement('p');
+      height.innerText= 'Altura: '+athletesList[i].height;
+      content.appendChild(height);
+
+      let weight= document.createElement('p');
+      weight.innerText= 'Peso: '+athletesList[i].weight;
+      content.appendChild(weight);
+
+      let sport= document.createElement('p');
+      sport.innerText= 'Deporte: '+athletesList[i].sport;
+      content.appendChild(sport);
+
+      let team= document.createElement('p');
+      team.innerText= 'Equipo: '+athletesList[i].team;
+      content.appendChild(team);
+
+      let noc= document.createElement('p');
+      noc.innerText= 'País: '+athletesList[i].noc;
+      content.appendChild(noc);
+
+      let age= document.createElement('p');
+      age.innerText= 'Edad: '+athletesList[i].age;
+      content.appendChild(age);
+
+      let event= document.createElement('p');
+      event.innerText= 'Disciplina: '+athletesList[i].event;
+      content.appendChild(event);
+
+      let medal= document.createElement('p');
+      medal.innerText= 'Medalla: '+athletesList[i].medal;
+      content.appendChild(medal);
+
+      card.appendChild(content);
+      root.appendChild(card);
+
+    }
+  }
+
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+export default dataFunctions;
