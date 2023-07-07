6ca3e39246a4bc1b8d96a84b3e989fe9cddc5d66
Andrea Cabrera <andreacabrera@MacBook-Air-de-Andrea-2.local>
2023-07-07 08:23:23 -0600

    Se agregó funcionalidad para buscar atletas por nombre

--- src/data.js
+++ src/data.js
@@ -137,61 +137,6 @@ const dataFunctions= {
     return allSports;
   },
 
-  // showAllCards: (athletesList)=>{
-  //   const root = document.getElementById('root');
- 
-  //   for(let i=0; i<athletesList.length; i++){
-  //     let card= document.createElement('div');
-  //     card.className= 'card';
-  //     let content= document.createElement('div');
-  //     content.className= 'content';
-
-  //     let name= document.createElement('p');
-  //     name.className = 'parrafoNombre';
-  //     name.innerText= 'Nombre: '+athletesList[i].name;
-  //     content.appendChild(name);
-
-  //     let gender= document.createElement('p');
-  //     gender.innerText= 'Género: '+athletesList[i].gender; 
-  //     content.appendChild(gender);
-
-  //     let height= document.createElement('p');
-  //     height.innerText= 'Altura: '+athletesList[i].height;
-  //     content.appendChild(height);
-
-  //     let weight= document.createElement('p');
-  //     weight.innerText= 'Peso: '+athletesList[i].weight;
-  //     content.appendChild(weight);
-
-  //     let sport= document.createElement('p');
-  //     sport.innerText= 'Deporte: '+athletesList[i].sport;
-  //     content.appendChild(sport);
-
-  //     let team= document.createElement('p');
-  //     team.innerText= 'Equipo: '+athletesList[i].team;
-  //     content.appendChild(team);
-
-  //     let noc= document.createElement('p');
-  //     noc.innerText= 'País: '+athletesList[i].noc;
-  //     content.appendChild(noc);
-
-  //     let age= document.createElement('p');
-  //     age.innerText= 'Edad: '+athletesList[i].age;
-  //     content.appendChild(age);
-
-  //     let event= document.createElement('p');
-  //     event.innerText= 'Disciplina: '+athletesList[i].event;
-  //     content.appendChild(event);
-
-  //     let medal= document.createElement('p');
-  //     medal.innerText= 'Medalla: '+athletesList[i].medal;
-  //     content.appendChild(medal);
-
-  //     card.appendChild(content);
-  //     root.appendChild(card);
-
-  //   }
-  // }
 
 };
 
