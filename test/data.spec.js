68d9a54a0dc4eb870d074283c26ba2029b60acab
Andrea Cabrera <andreacabrera@MacBook-Air-de-Andrea-2.local>
2023-07-07 14:47:50 -0600

    Se agregan los test de los filtros

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,54 @@
-import { example, anotherExample } from '../src/data.js';
+import dataFunctions from '../src/data.js';
 
+const dataTestGender = [{"gender": "M"},{ "gender": "F",}]
+ 
+const dataTestTeam = [{"team": "United States"}, {"team": "Italy"},{"team": "Canada"}]
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+const dataTestSport = [{"sport": "Rowing"},{"sport": "Taekwondo"}, {"sport": "Handball"}, {"sport": "Wrestling"}, {"sport": "Gymnastics"}, {"sport": "Swimming"}]
+
+describe ("dataFunctions",() =>{
+  //test para filtro de atletas femeninas
+  describe("dataFunctions.femaleFilter", () => {
+    it('Debería retornar sólo atletas mujeres para "' + dataTestGender + '"', () => {
+      expect(dataFunctions.femaleFilter(dataTestGender, "F")).toEqual([
+        {
+          "gender": "F",
+        }
+      ]);
+    });
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+
+  //test para filtro de atletas masculinos
+  describe("dataFunctions.maleFilter", () => {
+    it('Debería retornar sólo atletas hombres para "' + dataTestGender + '"', () => {
+      expect(dataFunctions.maleFilter(dataTestGender, "M")).toEqual([
+        {
+          "gender": "M",
+        }
+      ]);
+    });
+  });
+
+  //test para filtro de teams
+  describe("dataFunctions.teamFilter", () => {
+    it('Debería retornar sólo atletas del mismo equipo "' + dataTestTeam + '"', () => {
+      expect(dataFunctions.teamFilter(dataTestTeam, "Italy")).toEqual([
+        {
+          "team": "Italy",
+        }
+      ]);
+    });
+  });
+   //test para filtro de sports
+   describe("dataFunctions.sportFilter", () => {
+    it('Debería retornar sólo atletas del mismo deporte "' + dataTestSport + '"', () => {
+      expect(dataFunctions.sportFilter(dataTestSport, "Rowing")).toEqual([
+        {
+          "sport": "Rowing",
+        }
+      ]);
+    });
   });
 });
 
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
